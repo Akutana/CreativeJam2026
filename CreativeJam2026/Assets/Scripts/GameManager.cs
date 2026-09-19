7a83cbc49f25a5f8bbe207ca6b9f490cfef77513
Ewan <ewan.bchr0506@gmail.com>
2026-09-19 12:27:27 -0400

    Add win/game over and text for narration

--- CreativeJam2026/Assets/Scripts/GameManager.cs
+++ CreativeJam2026/Assets/Scripts/GameManager.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private ScreenFader screenFader;
     [SerializeField] private FPSController player;
+    [SerializeField] private TextMeshProUGUI transitionText;
+    [SerializeField] private TextMeshProUGUI narrationText;
 
     [SerializeField] private float blackScreenDuration = 2f;
-    int currentDay;
+    [SerializeField] private float transitionDuration = 3f;
+
+    int currentDay = -1;
 
     private void Start()
     {
-        StartCoroutine(StartDay(0));
+        transitionText.gameObject.SetActive(false);
+        narrationText.gameObject.SetActive(false);
+
+        screenFader.SetClear();
+
+        Debug.Log(currentDay);
     }
 
     private IEnumerator StartDay(int day)
@@ -26,11 +36,31 @@ public class GameManager : MonoBehaviour
         switch (day)
         {
             case 0:
+                yield return new WaitForSeconds(blackScreenDuration);
+
+                transitionText.text = "Day 0";
+                transitionText.gameObject.SetActive(true);
+
+                yield return new WaitForSeconds(transitionDuration);
+
+                transitionText.gameObject.SetActive(false);
+
                 yield return StartDay0();
+
                 break;
 
             case 1:
+                yield return new WaitForSeconds(blackScreenDuration);
+
+                transitionText.text = "Day 1";
+                transitionText.gameObject.SetActive(true);
+
+                yield return new WaitForSeconds(transitionDuration);
+
+                transitionText.gameObject.SetActive(false);
+
                 yield return StartDay1();
+
                 break;
         }
 
@@ -45,12 +75,17 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Starting day 0");
 
+        // show the context of the crime
+
         yield return null;
     }
 
     private IEnumerator StartDay1()
     {
         Debug.Log("Starting day 1");
+
+        // time loop day
+        // the player tries to find the murderer
 
         yield return null;
     }
@@ -65,6 +100,24 @@ public class GameManager : MonoBehaviour
 
         // do reset stuff
 
+        yield return new WaitForSeconds(blackScreenDuration);
+
+        transitionText.text = "Game Over";
+        transitionText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.text = "Day 1";
+        transitionText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.gameObject.SetActive(false);
+
         yield return StartDay1();
 
         yield return new WaitForSeconds(blackScreenDuration);
@@ -74,10 +127,53 @@ public class GameManager : MonoBehaviour
         player.SetInputEnabled(true);
     }
 
+    private IEnumerator Win()
+    {
+        Debug.Log("Win");
+
+        player.SetInputEnabled(false);
+
+        yield return screenFader.FadeIn();
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.text = "You found the murderer";
+        transitionText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.text = "Day 2";
+        transitionText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        transitionText.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(blackScreenDuration);
+
+        yield return screenFader.FadeOut();
+
+        transitionText.text = "Imagine this is the main menu";
+        transitionText.gameObject.SetActive(true);
+
+        player.SetInputEnabled(true);
+    }
+
     private void Update()
     {
         ////////////////// DEBUG ///////////////////
-        if (Input.GetKeyDown(KeyCode.N))
+        if (Input.GetKeyDown(KeyCode.B) &&
+            currentDay == -1)
+        {
+            StartCoroutine(StartDay(0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.N) &&
+            currentDay == 0)
         {
             StartCoroutine(StartDay(1));
         }
@@ -86,6 +182,26 @@ public class GameManager : MonoBehaviour
             currentDay == 1)
         {
             StartCoroutine(GameOver());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y) &&
+            currentDay == 1)
+        {
+            StartCoroutine(Win());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0) &&
+            currentDay != 0)
+        {
+            currentDay = 0;
+            Debug.Log("DEBUG - switching to day 0");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) &&
+            currentDay != 1)
+        {
+            currentDay = 1;
+            Debug.Log("DEBUG - switching to day 1");
         }
         ////////////////////////////////////////////
     }
