57cbe6cf4e3cd66baa0976ef15b2d2e4460ae4ff
Ewan <ewan.bchr0506@gmail.com>
2026-09-20 08:51:03 -0400

    Add buttons assets for murderer choice

--- CreativeJam2026/Assets/Scripts/GameManager.cs
+++ CreativeJam2026/Assets/Scripts/GameManager.cs
@@ -292,6 +292,8 @@ public class GameManager : MonoBehaviour
         button2.gameObject.SetActive(false);
         button3.gameObject.SetActive(false);
 
+        screenFader.SetClear();
+
         StartCoroutine(Win());
     }
 
@@ -304,6 +306,8 @@ public class GameManager : MonoBehaviour
         button1.gameObject.SetActive(false);
         button2.gameObject.SetActive(false);
         button3.gameObject.SetActive(false);
+
+        screenFader.SetClear();
 
         StartCoroutine(GameOver("The killer saw you accusing other suspects. He killed you."));
     }
@@ -330,6 +334,8 @@ public class GameManager : MonoBehaviour
         button1.gameObject.SetActive(true);
         button2.gameObject.SetActive(true);
         button3.gameObject.SetActive(true);
+
+        screenFader.SetScreenAlpha(0.65f);
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
